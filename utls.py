aaa39be3d3f98e79a569c36e841714c543fcc1d7
Boyko03 <boyko100@abv.bg>
2020-04-01 20:20:38 +0300

    Implement take_healing method.

--- utls.py
+++ utls.py
@@ -14,8 +14,16 @@ class Mixin:
     def take_damage(damage_points):
         pass
 
-    def take_healing(healing_points):
-        pass
+    def take_healing(self, healing_points):
+        if not self.is_alive():
+            return False
+
+        self.health += healing_points
+
+        if self.health > self.max_health:
+            self.health = self.max_health
+
+        return True
 
     def take_mana(mana_points):
         pass
