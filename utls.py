fe0e2f226b8a179a30a04134a62d03f3d2fcde8c
Boyko03 <boyko100@abv.bg>
2020-04-01 19:25:25 +0300

    Added mixin for hero and enemy

---
+++ utls.py
@@ -0,0 +1,30 @@
+class Mixin:
+    def is_alive():
+        pass
+
+    def can_cast():
+        pass
+
+    def get_health():
+        pass
+
+    def get_mana():
+        pass
+
+    def take_damage(damage_points):
+        pass
+
+    def take_healing(healing_points):
+        pass
+
+    def take_mana(mana_points):
+        pass
+
+    def attack(by):
+        pass
+
+    def equip(weapon):
+        pass
+
+    def learn(spell):
+        pass
