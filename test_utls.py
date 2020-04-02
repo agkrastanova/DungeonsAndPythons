40ab7fbd9964b59ba8abfc9e8b4711a80d448b36
Ann <krastanova98@gmail.com>
2020-04-02 12:29:41 +0300

    Add test_utls.py

---
+++ test_utls.py
@@ -0,0 +1,18 @@
+import unittest
+
+from utls import Mixin, Weapon, Spell
+
+
+class TestMixinClass(unittest.TestCase):
+    pass
+
+class TestWeaponClass(unittest.TestCase):
+    pass
+
+
+class TestSpellClass(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
