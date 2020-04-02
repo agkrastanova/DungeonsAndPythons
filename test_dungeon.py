27f623266e58f80e206a6083d11bcee9cc388c42
Ann <krastanova98@gmail.com>
2020-04-02 12:30:53 +0300

    Add dungeon test file

---
+++ test_dungeon.py
@@ -0,0 +1,15 @@
+import unittest
+
+from dungeon import Hero, Enemy
+
+
+class TestHeroClass(unittest.TestCase):
+    pass
+
+
+class TestEnemyClass(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
