a0422077b8377913e6fce1912496c51770824e1d
Boyko03 <boyko100@abv.bg>
2020-04-06 00:53:10 +0300

    Restore changes from previous commit.

--- dungeon.py
+++ dungeon.py
@@ -1,5 +1,63 @@
-from utls import Mixin, Weapon, Spell, Hero, Enemy, Fight
+from utls import Mixin, Weapon, Spell
 from random import shuffle
+
+
+class Hero(Mixin):
+    def __init__(self, name, title, health, mana, mana_regeneration_rate):
+        self.validate_arguments_hero(name, health, mana, mana_regeneration_rate)
+
+        self.name = name
+        self.title = title
+        self.health = health
+        self.mana = mana
+        self.mana_regeneration_rate = mana_regeneration_rate
+
+        self.max_health = health
+        self.max_mana = mana
+
+        self.weapon = None
+        self.spell = None
+
+    def known_as(self):
+        return(f'{self.name} the {self.title }')
+
+    @staticmethod
+    def validate_arguments_hero(name, health, mana, mana_regeneration_rate):
+
+        if type(name) is not str:
+            raise TypeError('Name should be string')
+
+        if type(health) is not int or health <= 0:
+            raise TypeError('Health should be positive integer')
+
+        if type(mana) is not int or mana <= 0:
+            raise TypeError('Mana should be positive integer')
+
+        if type(mana_regeneration_rate) is not int or mana_regeneration_rate <= 0:
+            raise TypeError('Mana_regeneration_rate should be positive integer')
+
+
+class Enemy(Mixin):
+    def __init__(self, health, mana, damage):
+        self.validate_arguments_enemy(health, mana, damage)
+
+        self.health = health
+        self.mana = mana
+        self.damage = damage
+        self.max_health = health
+        self.max_mana = mana
+
+    @staticmethod
+    def validate_arguments_enemy(health, mana, damage):
+
+        if type(health) is not int or health <= 0:
+            raise TypeError('Health should be positive integer')
+
+        if type(mana) is not int or mana <= 0:
+            raise TypeError('Mana should be positive integer')
+
+        if type(damage) is not int or damage <= 0:
+            raise TypeError('Damage should be positive integer')
 
 
 class Dungeon:
@@ -127,3 +185,19 @@ class Dungeon:
         if by == 'spell':
             row, col = self.find_hero()
             pass
+
+
+class Fight:
+    def __init__(self, hero, enemy, by=None):
+        if type(hero) is not Hero or type(enemy) is not Enemy:
+            raise TypeError('Invalid type.')
+        self.hero = hero
+        self.enemy = enemy
+        self.by = by
+
+        print(f'A fight is started between out Hero(health: {hero.health},',
+              f' mana: {hero.mana}) and Enemy(health: {enemy.health},',
+              f' mana: {enemy.mana}, damage: {enemy.damage}')
+
+    def fight(self):
+        pass
